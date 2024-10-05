3fe292e4f448d76feb9d2158ae65ae84933e5f5d
John Doe <aksel.martin.muru@ut.ee>
2024-10-05 14:58:31 +0300

    Star colors

--- scenes/world/stars/star.gd
+++ scenes/world/stars/star.gd
@@ -16,12 +16,26 @@ const MOUTH_TEXTURES := [
 	preload("res://scenes/world/stars/3.m.png"),
 	preload("res://scenes/world/stars/small.3.m.png"),
 ]
+const BACKGROUND_TEXTURES := [
+	preload("res://scenes/world/stars/background.small.png"),
+	preload("res://scenes/world/stars/background.circle.png"),
+]
+const COLORS := [
+	Color(0.85, 0.883, 0.913),
+	Color(0.85, 0.883, 0.913),
+	Color(0.918, 0.859, 0.897),
+	Color(0.854, 0.532, 0.535),
+	Color(0.893, 0.855, 0.699),
+]
 
 const DECEL := 400
 
+@onready var background: Sprite2D = $Face/Background
 @onready var mouth: Sprite2D = $Face/Mouth
 @onready var eyes: Sprite2D = $Face/Eyes
 @onready var blink_timer: Timer = $BlinkTimer
+@onready var glow := $Glow
+@onready var glow_front := $Glow2
 
 
 var _velocity: Vector2
@@ -33,10 +47,15 @@ func _ready() -> void:
 	blink_timer.timeout.connect(_blink_timer_timeout)
 	mouth.texture = MOUTH_TEXTURES.pick_random()
 	eyes.texture = EYE_TEXTURES.pick_random()
+	if randf() < 0.01:
+		background.texture = BACKGROUND_TEXTURES[1]
 	var scle := randf_range(0.4, 1.1)
 	scale = Vector2(scle, scle)
 	rotation = randf() * TAU
 	rotation_direction = randf_range(-1, 1)
+	var color: Color = COLORS.pick_random()
+	glow.modulate = color
+	glow_front.modulate = color
 
 
 func _physics_process(delta: float) -> void:
@@ -58,5 +77,7 @@ func apply_impulse(impulse: Vector2) -> void:
 
 func _blink_timer_timeout() -> void:
 	blink_timer.wait_time = 16 * randf()
-	eyes.hide()
-	create_tween().tween_interval(0.3).finished.connect(eyes.show)
+	var tw := create_tween()
+	tw.tween_property(eyes, "scale:y", 0.0, 0.01)
+	tw.tween_interval(0.3)
+	tw.tween_property(eyes, "scale:y", 1.0, 0.01)
