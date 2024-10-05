a5516ccc6be0c5ab4dc7915e8f1f24af3613d154
John Doe <aksel.martin.muru@ut.ee>
2024-10-05 15:43:49 +0300

    code style

--- scenes/gui/ingame_menus/panel_setter.gd
+++ scenes/gui/ingame_menus/panel_setter.gd
@@ -8,14 +8,17 @@ func _ready() -> void:
 	game_over_panel.visible = false
 	pause_panel.visible = false
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
 func _exit_tree() -> void:
 	GLOBAL.ui_layer = null
 
-func call_panel(gameover = false) -> void:
+
+func call_panel(gameover := false) -> void:
 	if (gameover): #If the game ends
 		game_over_panel.visible = true
 	else: #If the player presses the escape key.
