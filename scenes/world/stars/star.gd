ff8134d3eb3fda6aa8da7aa00e89c9a335e4573f
John Doe <aksel.martin.muru@ut.ee>
2024-10-05 14:37:08 +0300

    Star faces start

--- scenes/world/stars/star.gd
+++ scenes/world/stars/star.gd
@@ -1,9 +1,35 @@
 class_name Star extends Area2D
 
+const EYE_TEXTURES := [
+	preload("res://scenes/world/stars/1.png"),
+	preload("res://scenes/world/stars/1.small.png"),
+	preload("res://scenes/world/stars/2 (1).png"),
+	preload("res://scenes/world/stars/2.small.png"),
+	preload("res://scenes/world/stars/3.e.png"),
+	preload("res://scenes/world/stars/small.3.e.png"),
+	preload("res://scenes/world/stars/small.4.e.png"),
+]
+const MOUTH_TEXTURES := [
+	preload("res://scenes/world/stars/1..png"),
+	preload("res://scenes/world/stars/1.small.m.png"),
+	preload("res://scenes/world/stars/2 (2).png"),
+	preload("res://scenes/world/stars/3.m.png"),
+	preload("res://scenes/world/stars/small.3.m.png"),
+]
+
 const DECEL := 400
+
+@onready var mouth: Sprite2D = $Face/Mouth
+@onready var eyes: Sprite2D = $Face/Eyes
+
 
 var _velocity: Vector2
 var direction: Vector2
+
+
+func _ready() -> void:
+	mouth.texture = MOUTH_TEXTURES.pick_random()
+	eyes.texture = EYE_TEXTURES.pick_random()
 
 
 func _physics_process(delta: float) -> void:
