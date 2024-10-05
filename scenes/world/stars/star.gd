d7ed03fc127ad68098af3454e5392c50c06b20b7
John Doe <aksel.martin.muru@ut.ee>
2024-10-05 14:47:48 +0300

    stars blink

--- scenes/world/stars/star.gd
+++ scenes/world/stars/star.gd
@@ -21,15 +21,22 @@ const DECEL := 400
 
 @onready var mouth: Sprite2D = $Face/Mouth
 @onready var eyes: Sprite2D = $Face/Eyes
+@onready var blink_timer: Timer = $BlinkTimer
 
 
 var _velocity: Vector2
 var direction: Vector2
+var rotation_direction: float
 
 
 func _ready() -> void:
+	blink_timer.timeout.connect(_blink_timer_timeout)
 	mouth.texture = MOUTH_TEXTURES.pick_random()
 	eyes.texture = EYE_TEXTURES.pick_random()
+	var scle := randf_range(0.4, 1.1)
+	scale = Vector2(scle, scle)
+	rotation = randf() * TAU
+	rotation_direction = randf_range(-1, 1)
 
 
 func _physics_process(delta: float) -> void:
@@ -37,6 +44,8 @@ func _physics_process(delta: float) -> void:
 	
 	global_position += _velocity * delta
 	global_position += direction * delta
+	
+	rotation += rotation_direction * delta
 
 
 func set_velocity(velo: Vector2) -> void:
@@ -45,3 +54,9 @@ func set_velocity(velo: Vector2) -> void:
 
 func apply_impulse(impulse: Vector2) -> void:
 	_velocity += impulse
+
+
+func _blink_timer_timeout() -> void:
+	blink_timer.wait_time = 16 * randf()
+	eyes.hide()
+	create_tween().tween_interval(0.3).finished.connect(eyes.show)
