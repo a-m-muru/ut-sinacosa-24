d1f53477c19252a3e4b212cae0b62f299a67dd89
EgonVM <egonhea@gmail.com>
2024-10-05 15:35:21 +0300

    Added pause menu opening and closing
    With just a simple Esc press you can open and close the pause menu with ease.
    If it only had something extra...

--- autoload/global.gd
+++ autoload/global.gd
@@ -3,3 +3,11 @@ extends Node
 var remaining_stars := {}
 var star_noise := FastNoiseLite.new()
 var stars_vacuumed = 0
+var ui_layer = null
+var esc_pressed = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause_game"):
+		if ui_layer != null:
+			ui_layer.call_panel(false)
+		
---
+++ scenes/InGameMenus/panel_setter.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+@onready var pause_panel: Panel = $"Pause panel"
+@onready var game_over_panel: Panel = $"Game Over panel"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GLOBAL.ui_layer = self
+	game_over_panel.visible = false
+	pause_panel.visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _exit_tree() -> void:
+	GLOBAL.ui_layer = null
+
+func call_panel(gameover = false) -> void:
+	if (gameover): #If the game ends
+		game_over_panel.visible = true
+	else: #If the player presses the escape key.
+		if (pause_panel.visible):
+			pause_panel.visible = false
+		else:
+			pause_panel.visible = true
